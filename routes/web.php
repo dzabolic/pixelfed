cfa7c03bf72e6bd659c71322181d8ecc9acc0daf
dzabolic <thelesbiandemilovato@gmail.com>
2026-05-14 18:55:46 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -529,5 +529,6 @@ Route::domain(config('pixelfed.domain.app'))->middleware(['validemail', 'twofact
     Route::get('{username}', 'ProfileController@show');
     Route::get('stories/{username}/{id}', 'StoryController@show')->name('story.show');
     Route::post('/i/highlights/create', 'StatusController@createHighlight')->middleware('auth');
+    
 });
 
